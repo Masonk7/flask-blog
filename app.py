72e27b5bc94352aa1c6229b1251848179dca0f7b
Masonk7 <150885585+Masonk7@users.noreply.github.com>
2025-11-06 15:59:47 -0600

    code and html for delete post

--- app.py
+++ app.py
@@ -83,17 +83,26 @@ def edit(id):
         elif not content:
             flash('Content is required')
         else:
-           conn = get_db_connection
-           conn.execute('UPDATE posts SET title = ?, content = ? WHERE id = ?', (title, content, id)) 
-           conn.commit()
-           conn.close()
-           return redirect(url_for('index'))
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ? WHERE id = ?', (title, content, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
 
 
         
     return render_template('edit.html', post=post)
 
 
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    post = get_post(id)
+    conn = get_db_connection()
+    conn.execute('DELETE from posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"{}" was successfully deleted!'.format(post['title']))
+    return redirect (url_for('index'))
 
 
 app.run(port=5008)
